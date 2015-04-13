f6fa43dba4788a6abd38abab1df54602e6ca5834
zhangruichang <zhangruichang112@gmail.com>
2015-04-13 14:42:41 +0800

    BOP2015 Problem

---
+++ ProB/main.cpp
@@ -0,0 +1,101 @@
+/*
+Author: richard
+Contact: zhangruichang112@gmail.com
+*/
+#include<set>
+#include<map>
+#include<list>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<ctime>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<climits>
+#include<cstdlib>
+#include<cstring>
+#include<fstream>
+#include<sstream>
+#include<iostream>
+#include<algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+const int maxn = 1e6 + 10;
+typedef long long LL;
+typedef unsigned long long ULL;
+//int, -2^31~2^31-1    -2.1*10^9~2.1*10^9 (-2147483648-2147483647)
+//unsigned int, 0~2^32-1  0~4.2*10^9
+//long long  -2^63~2^63-1 -9*10^18~9*10^18
+//unsigned long long 0~2^64-1  0~1.8*10^19
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+const LL MOD=1e9+7;
+#define fi first
+#define se second
+int getint(){
+	int t = 0, flag = 1; char c = getchar();
+	while (c<'0' || c>'9' || c == '-')
+	{
+		if (c == '-')
+			flag = -1;
+		c = getchar();
+	}
+	while (c >= '0'&&c <= '9')
+		t = t * 10 + c - '0', c = getchar();
+	return t*flag;
+}
+int GCD(int m, int n)
+{
+    return !m ? n : GCD(n%m, m);
+}
+LL MultMod(LL a,LL b,LL MOD){
+    a %= MOD;
+    b %= MOD;
+    LL ret = 0;
+    while(b){
+        if (b & 1){
+            ret += a;
+            if(ret >= MOD) ret -= MOD;
+        }
+        a = a << 1;
+        if (a >= MOD) a -= MOD;
+        b = b >> 1;
+    }
+    return ret;
+}
+template <typename T>
+string to_string(T value)
+{
+  //create an output string stream
+  ostringstream os ;
+
+  //throw the value into the string stream
+  os << value ;
+
+  //convert the string stream into a string and return
+  return os.str() ;
+}
+
+LL n;
+int main()
+{
+/*
+#ifndef ONLINE_JUDGE
+    freopen ("in.txt" , "r" , stdin);
+    freopen ("out.txt" , "w" , stdout);
+#endif
+*/
+    int t;
+    cin>>t;
+    for(int ti=1;ti<=t;ti++)
+    {
+        cin>>n;
+        //cout<<MultMod(n, n+1, MOD)<<endl;
+        //cout<<MultMod(n,n, MOD)<<endl;
+        //cout<<MultMod(3LL, n, MOD)<<endl;
+        cout<<(MultMod(n, n+1, MOD)*MultMod(n, n+1, MOD)*( (MultMod(n,n, MOD)-MultMod(3LL, n, MOD)+4+MOD)%MOD)/8)%MOD<<endl;;
+    }
+	return 0;
+}
