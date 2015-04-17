e49efa18c6cac3991ef7474214a470c2765b1e0f
zhangruichang <zhangruichang112@gmail.com>
2015-04-17 23:10:49 +0800

    BOP Round Qualification

---
+++ RQProC/main.cpp
@@ -0,0 +1,116 @@
+/*
+Author: richard
+Contact: zhangruichang112@gmail.com
+*/
+#include<set>
+#include<map>
+#include<list>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<ctime>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<climits>
+#include<cstdlib>
+#include<cstring>
+#include<fstream>
+#include<sstream>
+#include<iostream>
+#include<algorithm>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+const int maxn = 100+10;
+typedef long long LL;
+typedef unsigned long long ULL;
+//int, -2^31~2^31-1    -2.1*10^9~2.1*10^9 (-2147483648-2147483647)
+//unsigned int, 0~2^32-1  0~4.2*10^9
+//long long  -2^63~2^63-1 -9*10^18~9*10^18
+//unsigned long long 0~2^64-1  0~1.8*10^19
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+#define fi first
+#define se second
+#define iOS  ios_base::sync_with_stdio(false)
+int getint(){
+	int t = 0, flag = 1; char c = getchar();
+	while (c<'0' || c>'9' || c == '-')
+	{
+		if (c == '-')
+			flag = -1;
+		c = getchar();
+	}
+	while (c >= '0'&&c <= '9')
+		t = t * 10 + c - '0', c = getchar();
+	return t*flag;
+}
+int GCD(int m, int n)
+{
+    return !m ? n : GCD(n%m, m);
+}
+template <typename T>
+string to_string(T value)
+{
+  //create an output string stream
+  ostringstream os ;
+
+  //throw the value into the string stream
+  os << value ;
+
+  //convert the string stream into a string and return
+  return os.str() ;
+}
+LL MultMod(LL a,LL b,LL MOD)
+{
+    a %= MOD;
+    b %= MOD;
+    LL ret = 0;
+    while(b){
+        if (b & 1){
+            ret += a;
+            if(ret >= MOD) ret -= MOD;
+        }
+        a = a << 1;
+        if (a >= MOD) a -= MOD;
+        b = b >> 1;
+    }
+    return ret;
+}
+int  n, t, m;
+struct
+{
+    int x, y;
+} A[maxn], B[maxn];
+int a, b;
+int f(int xi, int yj)
+{
+    int mandis=INT_MAX;
+    for(int i=0;i<b;i++) mandis=min(mandis, abs(xi-B[i].x)+abs(yj-B[i].y));
+    for(int i=0;i<a;i++) mandis+=(xi-A[i].x)*(xi-A[i].x)+(yj-A[i].y)*(yj-A[i].y);
+    return mandis;
+}
+int main()
+{
+/*
+#ifndef ONLINE_JUDGE
+    freopen ("in.txt" , "r" , stdin);
+    freopen ("out.txt" , "w" , stdout);
+#endif
+*/
+    cin>>t;
+    for(int ti=1;ti<=t;ti++)
+    {
+        cin>>n>>m>>a>>b;
+        for(int i=0;i<a;i++) cin>>A[i].x>>A[i].y;
+        for(int i=0;i<b;i++) cin>>B[i].x>>B[i].y;
+        int ans=INT_MAX;
+        for(int i=1;i<=n;i++) for(int j=1;j<=m;j++)
+        {
+            ans=min(ans, f(i, j));
+        }
+        printf("Case #%d: %d\n", ti, ans);
+    }
+	return 0;
+}
